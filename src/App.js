ebe05ab1ab5faf5a9e44391bc54ac6503acf467f
Donte Small <donte.small@zoom.us>
2023-01-05 18:11:35 -0500

    First Commit of lists_and_list_items section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
+const people = [{
+	name: 'John Doe',
+	age: 54,
+	hairColor: 'brown',
+	hobbies: ['swimming', 'bicycling', 'video games'],
+}, {
+	name: 'Brenda Smith',
+	age: 33,
+	hairColor: 'black',
+	hobbies: ['golf', 'mathematics'],
+}, {
+	name: 'Jane Garcia',
+	age: 27,
+	hairColor: 'blonde',
+	hobbies: ['biology', 'medicine', 'gymnastics'],
+}];
 
-import { SplitScreen } from './SplitScreen';
-
-const LeftHandComponent = () => { 
-  return <h1 style= {{backgroundColor: 'green'}}> Left</h1>
-};
-const RightHandComponent = () => {
-return <h1 style= {{ backgroundColor: 'red'}}> Right</h1>
-};
+const products = [{
+	name: 'Flat-Screen TV',
+	price: '$300',
+	description: 'Huge LCD screen, a great deal',
+	rating: 4.5,
+}, {
+	name: 'Basketball',
+	price: '$10',
+	description: 'Just like the pros use',
+	rating: 3.8,
+}, {
+	name: 'Running Shoes',
+	price: '$120',
+	description: 'State-of-the-art technology for optimum running',
+	rating: 4.2,
+}];
 
 function App() {
-  return (
-    <SplitScreen 
-        leftWeight= {1}
-        rightWeight= {3}> 
-        
-        <LeftHandComponent/>
-        <RightHandComponent/>
-   
-   </SplitScreen>  
-    
-    
-  );
+	return (
+		<h1>Your code goes here</h1>
+	);
 }
 
 export default App;
