fe09862561347855bc45439c6b8ae664d6015a4b
Donte Small <donte.small@zoom.us>
2023-01-05 15:35:45 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import { SplitScreen } from './SplitScreen';
+
+const LeftHandComponent = () => { 
+  return <h1 style= {{backgroundColor: 'green'}}> Left</h1>
+};
+const RightHandComponent = () => {
+return <h1 style= {{ backgroundColor: 'red'}}> Right</h1>
+};
+
+function App() {
+  return (
+    <SplitScreen left={LeftHandComponent}  right={RightHandComponent}>  </SplitScreen>
+    
+  );
+}
+
+export default App;
