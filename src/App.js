14716bbb701feb50dbb9058ac54d8c1cf5a6b574
Donte Small <donte.small@zoom.us>
2023-01-08 11:47:10 -0500

    generic editable resource pattern

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { printProps } from "./printProps";
 import { UserInfo} from "./UserInfo";
 import { withUser } from "./withUser";
+import { UserInfoForm } from "./UserInfoForm";
 
 const UserInfoWrapped = printProps(UserInfo);
 const UserInfoWithLoader = withUser(UserInfoWrapped, '234');
@@ -9,7 +10,9 @@ function App() {
 	return (
 		<>
 		<h1>Your code goes here</h1>
-		<UserInfoWithLoader />
+		{/* <UserInfoWithLoader /> */}
+
+		<UserInfoForm />
 		</>
 	);
 }
--- src/userInfoForm.js
+++ src/userInfoForm.js
@@ -1,28 +1,25 @@
 import { withEditableUser } from "./withEditableUser";
+import { withEditableResource } from "./withEditableResource";
 
-export const UserInfoForm = withEditableUser( ({user, onUserChange, onUserSave, onResetUser}) => {
-    const {name, age, hairColor } = user || {};
+export const UserInfoForm = withEditableResource(({ user, onChangeUser, onSaveUser, onResetUser }) => {
+	const { name, age, hairColor } = user || {};
 
-
-    return (
-        <div>
-            <h1>User Info</h1>
-            <div>
-                <label>First Name</label>
-                <input value={name} onChange={e => onUserChange({name: e.target.value})}/>
-            </div>
-            <div>
-                <label>Age</label>
-                <input type="number" value={age} onChange={e => onUserChange({age: e.target.value})}/>
-            </div>
-            <div>
-                <label>Hair Color </label>
-                <input value={hairColor} onChange={e => onUserChange({hairColor: Number(e.target.value)})}/>
-            </div>           
-            <div>
-                <button onClick={onUserSave}>Save</button>
-                <button onClick={onResetUser}>Reset</button>
-            </div>
-        </div>
-    );
-});
+	return user ? (
+		<>
+		<label>
+			Name:
+			<input value={name} onChange={e => onChangeUser({ name: e.target.value })} />
+		</label>
+		<label>
+			Age:
+			<input type="number" value={age} onChange={e => onChangeUser({ age: Number(e.target.value) })} />
+		</label>
+		<label>
+			Hair Color:
+			<input value={hairColor} onChange={e => onChangeUser({ hairColor: e.target.value })} />
+		</label>
+		<button onClick={onResetUser}>Reset</button>
+		<button onClick={onSaveUser}>Save Changes</button>
+		</>
+	) : <p>Loading...</p>;
+}, '/users/345', 'user');
